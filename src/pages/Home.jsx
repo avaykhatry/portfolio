09212d6bbe2767ac986ff159c827ffcaca007f40
avay <130426359+avaykhatry@users.noreply.github.com>
2025-10-31 23:20:13 +0545

    feat: add home page ui

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Button from "../components/common/Button";
+
+const Home = () => {
+    return (
+        <>
+            <div>Hi, my name is</div>
+            <div>Avaya Khatri.</div>
+            <div>I build things for the web.</div>
+            <p>I'm a frontend developer skilled in React, JavaScript, and responsive design. Passionate about crafting intuitive interfaces and continuously improving through real-world projects.</p>
+            <Button>My Resume</Button>
+        </>
+    )
+}
+
+export default Home;
